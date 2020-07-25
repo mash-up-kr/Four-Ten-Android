1dd1b6f51819b2c664e2abb9634ff0ced69e048c
taeiim <taeiim.god@gmail.com>
2020-07-25 23:13:38 +0900

    KoinExt 추가

---
+++ FourTen/app/src/main/java/com/mashup/fourten/JadoApplication.kt
@@ -0,0 +1,21 @@
+package com.mashup.fourten
+
+import android.app.Application
+import com.mashup.fourten.di.*
+import com.mashup.fourten.util.ext.setupKoin
+
+class JadoApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupKoin(
+            this,
+            viewModelModule,
+            networkModule,
+            repositoryModule,
+            remoteDataSourceModule,
+            localDataSourceModule
+        )
+    }
+
+}
