30a280d5c05636081bbc3b96a77def2c02d4e1b1
miinjung <laura126@ajou.ac.kr>
2020-09-08 14:05:41 +0900

    temp

---
+++ FourTen/app/src/main/java/com/mashup/fourten/ui/habitmake/HabitMakeActivity.kt
@@ -0,0 +1,12 @@
+package com.mashup.fourten.ui.habitmake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mashup.fourten.R
+
+class HabitMakeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_habit_make)
+    }
+}
